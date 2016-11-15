5f112f99cfefed401398eb9da7df5581aec43c1d
root <root@localhost.localdomain>
2016-11-15 15:46:33 +0000

    Initial commit of project. No functionality yet just getting things started.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Quiz from './app'; 
+
+
+		ReactDOM.render(
+		  (<Quiz />),
+		  document.getElementById('root')
+		);
